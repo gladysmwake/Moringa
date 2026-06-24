03e87b770359aa246f28c47dd65f014a13d76452
GladysMwake <gladysk2004@gmail.com>
2026-06-24 11:03:50 +0300

    learnt about user inputs on the if statements....and i tried nesting an if statement in another if statement

---
+++ js/intro/user_input_if.js
@@ -0,0 +1,36 @@
+let age= prompt("Enter your age:")
+let gender= prompt("Enter your gender:")
+
+alert(`You entered ${age}`);
+alert(`Your gender is ${gender}`);
+
+if(age<10){
+    alert(`You are a baby.`)
+}
+
+if(age>=10 && age<= 17){
+    alert(`You are a teen`)
+}
+
+if(age>18 && age<=26 ){
+    alert(`You are a young adult`)
+}
+
+if(age>27 && age <=44){
+    alert(`You can start looking for a partner to settle down with`)
+}
+
+if(age>45 && age <=54){
+    alert(`You should be thinking of retirement`)
+}
+
+if(age>=55){
+    if (gender === "male") {
+        alert("You are a guka!");
+    } else if (gender === "female") {
+        alert("You are a shosh!");
+    } else {
+        alert("You are a senior!");
+    }
+    
+}
