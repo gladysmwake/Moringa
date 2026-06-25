773d82fc21cb2ee119f63e219f5abc55c42adc2f
GladysMwake <gladysk2004@gmail.com>
2026-06-25 10:58:29 +0300

    tried building a calculator

--- js/intro/index.js
+++ js/intro/index.js
@@ -1,4 +1,37 @@
-let x=34;
-        let y=20;
-        let z=x+y;
-        console.log(z);
+let grossMonthlySalary = null;
+
+while (true) {
+    let gross = prompt("Enter your gross monthly salary:");
+    if (!isNaN(gross)) {
+        gross = Number(gross);
+        if (gross > 0) {
+            grossMonthlySalary = gross;
+            break;
+        }
+    }
+    alert(`Invalid amount entered.
+            Ensure you enter a number greater than 0`);
+}
+
+let paye = 0;
+
+if (grossMonthlySalary >= 0 && grossMonthlySalary <= 24000) {
+    paye = grossMonthlySalary * 0.10;
+
+} else if (grossMonthlySalary >= 24001 && grossMonthlySalary <= 32333) {
+    paye = grossMonthlySalary * 0.25;
+
+} else if (grossMonthlySalary >= 32334 && grossMonthlySalary <= 500000) {
+    paye = grossMonthlySalary * 0.30;
+
+} else if (grossMonthlySalary >= 500001 && grossMonthlySalary <= 800000) {
+    paye = grossMonthlySalary * 0.325;
+
+} else if (grossMonthlySalary > 800000) {
+    paye = grossMonthlySalary * 0.35;
+}
+
+
+alert(`Your gross salary: KES ${grossMonthlySalary}
+Your PAYE deduction: KES ${paye}
+Net salary: KES ${grossMonthlySalary - paye}`);
