bcf5a35ba3c201754204f1e89634beab4ad74410
GladysMwake <gladysk2004@gmail.com>
2026-06-30 15:30:06 +0300

    learnt about scopes...local and global

---
+++ js/junior-dev/scope.js
@@ -0,0 +1,28 @@
+let name= "XOXO";
+
+function sayMyName(){
+    let name = "Cutie";
+
+    console.log(`Your name is ${name} line ${5}`);
+
+    if (10===10){
+        let name = "Shawdy";
+        console.log(`Your name is ${name} line ${9}`);
+
+        if (23==="23"){
+            let name = "Munchkin";
+            console.log(`Your name is ${name} line ${13}`)
+        }
+        else {
+            alert(`HEY POOKIE`);
+        }
+    }
+    //let name ="Aniya";
+    console.log(`Your name is ${name} line ${21}`);
+
+
+
+
+
+}
+sayMyName()
